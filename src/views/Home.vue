1b9cb38205cf0356e3efb155acdbb1a6f819c3d2
yangpu <yangpu@xlink.cn>
2019-05-08 12:45:00 +0800

    feat(项目配置):

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search" dot>标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o" info="5">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o" info="20">标签</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+@Component
+export default class Home extends Vue {
+  active: number = 0;
+}
 </script>
