5f4ac909372f230b2c7bbb91627eb3dad6bfb7f0
yangpu <18312701884@163.com>
2019-05-08 20:24:56 +0800

    feat：添加按钮

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="about">
-    <h2 class="education-block__title">titl</h2>
+    <h2 class="education-block__title">
+      请绑定手机号
+    </h2>
     <van-cell-group>
       <van-field v-model="phone" label="手机号" placeholder="请输入手机号" />
     </van-cell-group>
-    <van-button type="primary" size="large" class="mt20">主要按钮</van-button>
+    <van-button type="primary" size="large" class="mt20">绑定手机</van-button>
   </div>
 </template>
 <script lang="ts">
@@ -18,8 +20,9 @@ export default class About extends Vue {
 <style lang="scss" scoped>
 // @import '~@/assets/scss/utils.scss';
 .about {
-  margin: 0 auto;
-  width: 90%;
+  // margin: 0 auto;
+  // width: 90%;
+  padding: 0 20px;
   background-color: #f2f3f5;
 }
 
