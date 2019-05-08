8cc1940bc893b272ca59a70192a65a1aa7c0cfb7
yangpu <18312701884@163.com>
2019-05-08 20:20:09 +0800

    feat:添加sass的变量

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+// vue.config.js
+module.exports = {
+  // options...
+  css: {
+    loaderOptions: {
+      sass: {
+        // 会在所有的sass文件添加相同的代码，变量，mixins之类的，否则假设有20个文件被loader处理就有20个被导入，批量导入
+        // 但在被import的文件里面不会的
+        data: `@import '@/assets/scss/variable.scss';`
+      },
+    }
+  }
+}
