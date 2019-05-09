6c2ed1ed5f40896cc715e1daa1f5fa588651f813
yangpu <yangpu@xlink.cn>
2019-05-09 18:37:39 +0800

    feat:添加table

--- src/views/course/index.vue
+++ src/views/course/index.vue
@@ -1,26 +1,16 @@
 <template>
-  <div class="about">
+  <div class="education-warper bg">
     <mu-container>
-      <mu-tabs :value.sync="active" inverse color="secondary" text-color="rgba(0, 0, 0, .54)"  center>
-        <mu-tab>TAB ITEM 1</mu-tab>
-        <mu-tab>TAB ITEM 2</mu-tab>
-        <mu-tab>TAB ITEM 3</mu-tab>
-      </mu-tabs>
-      <div class="demo-text" v-if="active === 0">
-        <p>“……是的，我承认从很早以前我都害怕死亡，在医院里被人研究的时候，以前做出布局的时候，其实我很怕死的，因为除了死以外，我没有值得珍惜的东西，很奇怪是吧？恰好是这样我真的很怕死，因为我想要有值得珍惜的东西之后，死亡之后才会不寂寞，我才能够安然的面对死亡……”</p>
-      </div>
-      <div class="demo-text" v-if="active === 1">
-        <p>“我的心从来没有这么坚定过，所以我会为了补偿而死，也可以为了补偿而死……一辈子，急辈子都无所谓，我绝不后退！”</p>
-        <p>“如果我后退呢？如果我想要死呢？我不想你再次背对着我逃跑了……”</p>
-        <p>“那么就去地狱找到你，我绝对不逃！”</p>
-        <p>“白痴，你也哭了？因为那些软弱拖累你的脚步？”</p>
-      </div>
-      <div class="demo-text" v-if="active === 2">
-        <p>“不，这泪水……是因为勇气，因为力量，因为信任，……你不会懂的！”</p>
-        <p>“我不会帮你，想要什么样的未来……自己去追寻吧！”</p>
-        <p>“我不需要你的帮忙！未来我会一手开启，什么样的敌人我也不会惧怕……还有，其实我们可以成为朋友的……”</p>
-        <p>“也许吧，未来……给你了。”</p>
-      </div>
+      <mu-paper :z-depth="1">
+        <mu-data-table :loading="loading" :columns="columns" :data="list">
+          <template slot-scope="scope">
+            <td>{{scope.row.name}}</td>
+            <td class="is-right">{{scope.row.calories}}</td>
+            <td class="is-right">{{scope.row.fat}}</td>
+            <td class="is-right">{{scope.row.carbs}}</td>
+          </template>
+        </mu-data-table>
+      </mu-paper>
     </mu-container>
   </div>
 </template>
@@ -29,7 +19,41 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Course extends Vue {
-  public active: number = 0;
+  private loading: boolean = false;
+
+  private columns: any[] = [
+    { title: 'Dessert (100g serving)', name: 'name' },
+    { title: 'Calories', name: 'calories', align: 'center' },
+    { title: 'Fat (g)', name: 'fat', align: 'center' },
+    { title: 'Carbs (g)', name: 'carbs', align: 'center' },
+  ];
+
+  private list: any[] = [
+    {
+      name: 'Frozen Yogurt',
+      calories: 159,
+      fat: 6.0,
+      carbs: 24,
+      protein: 4.0,
+      iron: 1,
+    },
+    {
+      name: 'Ice cream sandwich',
+      calories: 237,
+      fat: 9.0,
+      carbs: 37,
+      protein: 4.3,
+      iron: 1,
+    },
+    {
+      name: 'Eclair',
+      calories: 262,
+      fat: 16.0,
+      carbs: 23,
+      protein: 6.0,
+      iron: 7,
+    },
+  ];
 }
 </script>
 <style lang="scss">
