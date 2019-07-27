ec45ee710b4228dcf23152d0dd9da3122c7f0600
yangpu <1325896386@qq.com>
2019-07-27 14:16:33 +0800

    feat(api): 添加学生stage的包

---
+++ src/api/student-package.ts
@@ -0,0 +1,44 @@
+import {IStudentPackage} from '../const/type/student-package';
+import { ApiListData, ApiResponse } from '../types/api';
+import http from '../utils/http';
+
+
+
+
+/**
+ * 添加学员课程包
+ * @param {*} params
+ */
+export function addStudentPackage(params: IStudentPackage) {
+    return http.post('student-package', params);
+}
+
+/**
+ *
+ * @param params 查询参数
+ */
+export function getStudentPackageList(params: any): ApiListData<IStudentPackage> {
+    return http.post('student-package/list', params);
+}
+
+/**
+ *
+ * @param id 学员课程包的id
+ */
+export function getPackage(id: string): ApiResponse<IStudentPackage> {
+    return http.get(`student-package/${id}`);
+}
+
+/**
+ *
+ * @param id 学员课程包id
+ * @param params
+ */
+export function updateStudentPackage(id: string, params: Partial<IStudentPackage>): ApiResponse<IStudentPackage> {
+    return http.put(`student-package/${id}`, params);
+}
+
+
+export function delStudentPackage(id: string): ApiResponse<IStudentPackage> {
+    return http.delete(`student-package/${id}`);
+}
---
+++ src/const/type/student-package.ts
@@ -0,0 +1,15 @@
+export interface IStudentPackage {
+  readonly _id?: string;
+  studentIds: string[]; // 学生
+  packageId: string; // 课程包id
+  activeTime: string; // 激活时间
+  endTime: string; // 结束时间
+  amount: number; // 价格
+  count: number; // 总数量
+  surplus: number; // 剩余课时
+  used: number; // 使用课时
+  isActive: boolean; // 是否激活
+  beOverdue: boolean; // // 是否过时
+  isPush: boolean; // 是否推送过期的通知
+  period: number;
+}
