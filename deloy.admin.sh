33ec93318a6a4e7acf8a778ad3365eb650879525
yangpu <yangpu@xiao100.com>
2020-12-03 16:22:41 +0800

    feat: 完成整个绑定的闭环

---
+++ deloy.admin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 部署的服务器的地址
+deloy_server=root@118.31.227.99
+
+# 前端部署的地址
+deloy_root_path=/root/docker/nginx/html/
+
+# 部署的目录
+deloy_path=wechat
+
+
+target=${deloy_server}:${deloy_root_path}$deloy_path/
+echo "拷贝文件夹到目录下:${target}"
+scp -i ~/.ssh/pk_rsa.pem -r public/admin.html ${target}
+
+
